6146a500227f9f46eaca732f1bb80f21d781ac4b
EstebanPerilla <alvaroeestebanperillajimenez@gmail.com>
2021-10-30 18:42:43 -0500

    deployment changes

--- authApp/models/sale.py
+++ authApp/models/sale.py
@@ -3,7 +3,7 @@ from .invoice import Invoice
 from .product import Product
 
 class Sale(models.Model):
-    idSale = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     product = models.ForeignKey(Product, related_name='sale',on_delete=models.SET_NULL,null=True)
     invoice = models.ForeignKey(Invoice, related_name='sale',on_delete=models.SET_NULL,null=True)
     paymentMethod = models.CharField('PaymentMethod', max_length = 30)
