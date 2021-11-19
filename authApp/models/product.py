619c4c7e443e17a17c4f6e2651acbb58a779d540
Laura Roa <lauramilena_92@hotmail.com>
2021-11-19 16:48:36 -0500

    P43G6-15 Adición de campos en ms Inventario

--- authApp/models/product.py
+++ authApp/models/product.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Product(models.Model):
-    id        = models.AutoField(primary_key=True)
-    name      = models.CharField('Name',max_length = 30)
-    price     = models.FloatField('Price')
-    amount    = models.IntegerField()
+    id                = models.AutoField(primary_key=True)
+    name              = models.CharField('Name',max_length = 30)
+    price             = models.FloatField('Price')
+    initial_stock     = models.IntegerField(default=0)
+    stock_in          = models.IntegerField(default=0)
+    stock_out         = models.IntegerField(default=0)
+    final_stock       = models.IntegerField(default=0)
